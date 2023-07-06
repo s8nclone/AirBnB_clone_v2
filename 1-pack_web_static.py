93b3999201a7f588f62fad07fa9764ddd3360164
alx <atundeg@gmail.com>
2023-07-06 13:02:33 +0100

    create fabric script that archives web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+Generates a .tgz archive from the contents of the web_static folder
+
+function: do_pack()
+"""
+from fabric import tasks
+import datetime
+
+
+@tasks
+def do_pack(c):
+    """Generates a .tgz archive and saves with current date
+    """
+    current_time = datetime.datetime.utcnow().strftime(
+        "%Y%m%d%H%M%S")
+    archive_name = f"archive_{current_time}.tgz"
+    c.local(f"mkdir -p versions")
+    c.local(f"tar -czvf versions/{archive_name} -C web_static .")
