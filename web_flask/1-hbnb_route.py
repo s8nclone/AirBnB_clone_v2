00ffae46c376650cf0322b0d49c896ca985902f0
alx <atundeg@gmail.com>
2023-07-22 13:27:07 +0100

    add another route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""simple flask script
+
+Returns:
+    _type_: _description_
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def sisplay_hbnb():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    # Start the Flask development server on 0.0.0.0:5000
+    app.run(host="0.0.0.0", port=5000, debug=True)
